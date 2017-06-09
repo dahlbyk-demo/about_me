611f87019e438c471f65ee05c03eef62c77c0cd3
Keith Dahlby <keith@newbo.co>
2017-06-09 11:33:41 -0500

    Fix linting and add whatever function

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log("Loaded app.js")
+console.log('Loaded app.js');
+
+function whatever(input) {
+  if (!input) {
+    console.log('invalid input');
+    return;
+  }
+
+  return input.substring(0,1);
+}
